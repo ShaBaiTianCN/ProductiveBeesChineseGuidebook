a6ce6f35673a4a0ebeb3b4ef197b30ee9d649d1c
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2022-05-26 20:30:00 +0800

    set mod info

---
+++ src/main/java/com/anyijun/productivebeeschineseguidebook/ProductiveBeesChineseGuidebook.java
@@ -0,0 +1,7 @@
+package com.anyijun.productivebeeschineseguidebook;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("productivebeeschineseguidebook")
+public class ProductiveBeesChineseGuidebook {
+}
